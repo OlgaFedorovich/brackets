6784f824206ec41ee720e2606462d03c18669269
OlgaFedorovich <63749183+OlgaFedorovich@users.noreply.github.com>
2020-10-25 17:17:51 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const open = ['(', '{', '[', '|', '1', '3', '5', '7', '8'],
+      close = [')', '}', ']', '|', '2', '4', '6', '7', '8'],
+  stack =[];
+  console.log(str);
+
+  for(let i = 0; i < str.length; i++) {
+    if ((str[i] === '|' || str[i] === '7' || str[i] === '8' ) && (close.indexOf(str[i]) === open.indexOf(stack[stack.length - 1]))) {
+      stack.pop();
+    } else if((str[i] === '|' || str[i] === '7' || str[i] === '8') && str[i] === str[i+1]) {i++}
+    else if(open.includes(str[i])) {
+      stack.push(str[i]);
+    } else {
+      if(close.indexOf(str[i]) === open.indexOf(stack[stack.length - 1])) {
+        stack.pop();
+      } else return false;
+    }
+    console.log(stack);
+  }
+
+  
+
+  return stack.length === 0;
+  
 }
